d9b5545e754f41f0c4b22d264a509534aa18c4f4
Алексей Старченко <Prolex>
2023-01-16 15:30:15 +0300

    задача 3 решена

---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+double [] arr = {7.12, 10.2, 5, 2.55, 22.5,};
+double min = arr[0];
+double max = arr[0];
+double diff = 0;
+Console.WriteLine("[{0}]", string.Join(", ", arr)); // Для наглядности выводим все элементы массив
+if(arr[1] > max) max = arr[1];
+if(arr[2] > max) max = arr[2];
+if(arr[3] > max) max = arr[3];
+if(arr[4] > max) max = arr[4];
+if(arr[1] < min) min = arr[1];
+if(arr[2] < min) min = arr[2];
+if(arr[3] < min) min = arr[3];
+if(arr[4] < min) min = arr[4];
+Console.WriteLine($"разница между максимальным и минимальным числом массива равна {diff = max - min} ");
